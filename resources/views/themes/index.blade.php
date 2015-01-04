15a46c0067dbc0027b5e3db9658d13c74ce483af
nicklaw5 <nick_law@tpg.com.au>
2015-01-04 22:24:13 +1100

    added themes

--- resources/views/themes/index.blade.php
+++ resources/views/themes/index.blade.php
@@ -20,7 +20,7 @@
             <i class="icon-info block m-b-xs fa-2x"></i>
             <span>About</span>
           </a>
-          <a href class="col padder-v text-muted">
+          <a target="_blank" href="http://krazicode.com/themeforest/html/closer/index-red.html" class="col padder-v text-muted">
             <i class="icon-link block m-b-xs fa-2x"></i>
             <span>Website</span>
           </a>
@@ -46,7 +46,7 @@
             <i class="icon-info block m-b-xs fa-2x"></i>
             <span>About</span>
           </a>
-          <a href class="col padder-v text-muted">
+          <a target="_blank" href="http://html.nootheme.com/citilights/" class="col padder-v text-muted">
             <i class="icon-link block m-b-xs fa-2x"></i>
             <span>Website</span>
           </a>
@@ -71,7 +71,7 @@
             <i class="icon-info block m-b-xs fa-2x"></i>
             <span>About</span>
           </a>
-          <a href class="col padder-v text-muted">
+          <a target="_blank" href="http://tlazya.com/krown/index.html" class="col padder-v text-muted">
             <i class="icon-link block m-b-xs fa-2x"></i>
             <span>Website</span>
           </a>
@@ -96,7 +96,7 @@
             <i class="icon-info block m-b-xs fa-2x"></i>
             <span>About</span>
           </a>
-          <a href class="col padder-v text-muted">
+          <a target="_blank" href="http://designingmedia.com/html/enmit/index_fullslider.html" class="col padder-v text-muted">
             <i class="icon-link block m-b-xs fa-2x"></i>
             <span>Website</span>
           </a>
@@ -121,7 +121,7 @@
             <i class="icon-info block m-b-xs fa-2x"></i>
             <span>About</span>
           </a>
-          <a href class="col padder-v text-muted">
+          <a target="_blank" href="http://burak-aydin.com/html/caffe/" class="col padder-v text-muted">
             <i class="icon-link block m-b-xs fa-2x"></i>
             <span>Website</span>
           </a>
@@ -146,7 +146,7 @@
             <i class="icon-info block m-b-xs fa-2x"></i>
             <span>About</span>
           </a>
-          <a href class="col padder-v text-muted">
+          <a target="_blank" href="http://moonart.net.ua/freedom/index.html" class="col padder-v text-muted">
             <i class="icon-link block m-b-xs fa-2x"></i>
             <span>Website</span>
           </a>
@@ -154,6 +154,155 @@
       </div>
     </div>
 
+    <div class="col-md-4">
+      <div class="panel b-a">
+        <div class="panel-heading no-border bg-info">          
+          <span class="text-lt">Theme 7</span>
+        </div>
+        <div class="item m-l-n-xxs m-r-n-xxs">
+          <img src="{{ asset('img/themes/theme-7.png') }}" class="img-full">
+        </div>
+        <div class="hbox text-center text-sm">          
+          <a href class="col padder-v text-muted b-r b-light">
+            <i class="icon-plus block m-b-xs fa-2x"></i>
+            <span>New Project</span>
+          </a>
+          <a href class="col padder-v text-muted b-r b-light">
+            <i class="icon-info block m-b-xs fa-2x"></i>
+            <span>About</span>
+          </a>
+          <a target="_blank" href="http://www.entiri.net/prevrat/v1.1/" class="col padder-v text-muted">
+            <i class="icon-link block m-b-xs fa-2x"></i>
+            <span>Website</span>
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-4">
+      <div class="panel b-a">
+        <div class="panel-heading no-border bg-info">          
+          <span class="text-lt">Theme 8</span>
+        </div>
+        <div class="item m-l-n-xxs m-r-n-xxs">
+          <img src="{{ asset('img/themes/theme-8.png') }}" class="img-full">
+        </div>
+        <div class="hbox text-center text-sm">          
+          <a href class="col padder-v text-muted b-r b-light">
+            <i class="icon-plus block m-b-xs fa-2x"></i>
+            <span>New Project</span>
+          </a>
+          <a href class="col padder-v text-muted b-r b-light">
+            <i class="icon-info block m-b-xs fa-2x"></i>
+            <span>About</span>
+          </a>
+          <a target="_blank" href="http://enfusionthemes.com/demo/cuvey-html/main-html/index-dark.html" class="col padder-v text-muted">
+            <i class="icon-link block m-b-xs fa-2x"></i>
+            <span>Website</span>
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-4">
+      <div class="panel b-a">
+        <div class="panel-heading no-border bg-info">          
+          <span class="text-lt">Theme 9</span>
+        </div>
+        <div class="item m-l-n-xxs m-r-n-xxs">
+          <img src="{{ asset('img/themes/theme-9.png') }}" class="img-full">
+        </div>
+        <div class="hbox text-center text-sm">          
+          <a href class="col padder-v text-muted b-r b-light">
+            <i class="icon-plus block m-b-xs fa-2x"></i>
+            <span>New Project</span>
+          </a>
+          <a href class="col padder-v text-muted b-r b-light">
+            <i class="icon-info block m-b-xs fa-2x"></i>
+            <span>About</span>
+          </a>
+          <a target="_blank" href="http://fmedias.com/win/index.html#!home" class="col padder-v text-muted">
+            <i class="icon-link block m-b-xs fa-2x"></i>
+            <span>Website</span>
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-4">
+      <div class="panel b-a">
+        <div class="panel-heading no-border bg-info">          
+          <span class="text-lt">Theme 10</span>
+        </div>
+        <div class="item m-l-n-xxs m-r-n-xxs">
+          <img src="{{ asset('img/themes/theme-10.png') }}" class="img-full">
+        </div>
+        <div class="hbox text-center text-sm">          
+          <a href class="col padder-v text-muted b-r b-light">
+            <i class="icon-plus block m-b-xs fa-2x"></i>
+            <span>New Project</span>
+          </a>
+          <a href class="col padder-v text-muted b-r b-light">
+            <i class="icon-info block m-b-xs fa-2x"></i>
+            <span>About</span>
+          </a>
+          <a target="_blank" href="http://vasterad.com/themes/chow/index-2.html" class="col padder-v text-muted">
+            <i class="icon-link block m-b-xs fa-2x"></i>
+            <span>Website</span>
+          </a>
+        </div>
+      </div>
+    </div>    
+
+    <div class="col-md-4">
+      <div class="panel b-a">
+        <div class="panel-heading no-border bg-info">          
+          <span class="text-lt">Theme 11</span>
+        </div>
+        <div class="item m-l-n-xxs m-r-n-xxs">
+          <img src="{{ asset('img/themes/theme-11.png') }}" class="img-full">
+        </div>
+        <div class="hbox text-center text-sm">          
+          <a href class="col padder-v text-muted b-r b-light">
+            <i class="icon-plus block m-b-xs fa-2x"></i>
+            <span>New Project</span>
+          </a>
+          <a href class="col padder-v text-muted b-r b-light">
+            <i class="icon-info block m-b-xs fa-2x"></i>
+            <span>About</span>
+          </a>
+          <a target="_blank" href="http://kl-webmedia.com/demo/snaps/Snaps/orange/index.html" class="col padder-v text-muted">
+            <i class="icon-link block m-b-xs fa-2x"></i>
+            <span>Website</span>
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-4">
+      <div class="panel b-a">
+        <div class="panel-heading no-border bg-info">          
+          <span class="text-lt">Theme 12</span>
+        </div>
+        <div class="item m-l-n-xxs m-r-n-xxs">
+          <img src="{{ asset('img/themes/theme-12.png') }}" class="img-full">
+        </div>
+        <div class="hbox text-center text-sm">          
+          <a href class="col padder-v text-muted b-r b-light">
+            <i class="icon-plus block m-b-xs fa-2x"></i>
+            <span>New Project</span>
+          </a>
+          <a href class="col padder-v text-muted b-r b-light">
+            <i class="icon-info block m-b-xs fa-2x"></i>
+            <span>About</span>
+          </a>
+          <a target="_blank" href="http://acrocapoeira.com/zero/index-slider-parallax.html" class="col padder-v text-muted">
+            <i class="icon-link block m-b-xs fa-2x"></i>
+            <span>Website</span>
+          </a>
+        </div>
+      </div>
+    </div>
 
 </div>
 @stop
